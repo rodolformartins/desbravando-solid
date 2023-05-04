ffd5ba6623dd416e3bc47562cf4ccdc5d594cc77
rodolformartins <rodolform@protonmail.com>
2023-05-04 09:15:43 -0300

    change main file

--- cotuba/src/main/java/cotuba/Main.java
+++ cotuba/src/main/java/cotuba/Main.java
@@ -1,23 +1,15 @@
 package cotuba;
 
-import java.io.File;
+
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
-import java.nio.file.Paths;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+
 import org.commonmark.node.AbstractVisitor;
 import org.commonmark.node.Heading;
 import org.commonmark.node.Node;
@@ -42,36 +34,6 @@ import nl.siegmann.epublib.service.MediatypeService;
 public class Main {
 
   public static void main(String[] args) {
-    var options = new Options();
-
-    var opcaoDeDiretorioDosMD = new Option("d", "dir", true,
-        "Diretório que contém os arquivos md. Default: diretório atual.");
-    options.addOption(opcaoDeDiretorioDosMD);
-
-    var opcaoDeFormatoDoEbook = new Option("f", "format", true,
-        "Formato de saída do ebook. Pode ser: pdf ou epub. Default: pdf");
-    options.addOption(opcaoDeFormatoDoEbook);
-
-    var opcaoDeArquivoDeSaida = new Option("o", "output", true,
-        "Arquivo de saída do ebook. Default: book.{formato}.");
-    options.addOption(opcaoDeArquivoDeSaida);
-
-    var opcaoModoVerboso = new Option("v", "verbose", false,
-        "Habilita modo verboso.");
-    options.addOption(opcaoModoVerboso);
-
-    CommandLineParser cmdParser = new DefaultParser();
-    var ajuda = new HelpFormatter();
-    CommandLine cmd;
-
-    try {
-      cmd = cmdParser.parse(options, args);
-    } catch (ParseException e) {
-      System.err.println(e.getMessage());
-      ajuda.printHelp("cotuba", options);
-      System.exit(1);
-      return;
-    }
 
     Path diretorioDosMD;
     String formato;
@@ -80,41 +42,11 @@ public class Main {
 
     try {
 
-      String nomeDoDiretorioDosMD = cmd.getOptionValue("dir");
-
-      if (nomeDoDiretorioDosMD != null) {
-        diretorioDosMD = Paths.get(nomeDoDiretorioDosMD);
-        if (!Files.isDirectory(diretorioDosMD)) {
-          throw new IllegalArgumentException(nomeDoDiretorioDosMD + " não é um diretório.");
-        }
-      } else {
-        Path diretorioAtual = Paths.get("");
-        diretorioDosMD = diretorioAtual;
-      }
-
-      String nomeDoFormatoDoEbook = cmd.getOptionValue("format");
-
-      if (nomeDoFormatoDoEbook != null) {
-        formato = nomeDoFormatoDoEbook.toLowerCase();
-      } else {
-        formato = "pdf";
-      }
-
-      String nomeDoArquivoDeSaidaDoEbook = cmd.getOptionValue("output");
-      if (nomeDoArquivoDeSaidaDoEbook != null) {
-        arquivoDeSaida = Paths.get(nomeDoArquivoDeSaidaDoEbook);
-      } else {
-        arquivoDeSaida = Paths.get("book." + formato.toLowerCase());
-      }
-      if (Files.isDirectory(arquivoDeSaida)) {
-        // deleta arquivos do diretório recursivamente
-        Files.walk(arquivoDeSaida).sorted(Comparator.reverseOrder())
-            .map(Path::toFile).forEach(File::delete);
-      } else {
-        Files.deleteIfExists(arquivoDeSaida);
-      }
-
-      modoVerboso = cmd.hasOption("verbose");
+      var opcoesCLI = new LeitorOpcoesCLI(args);
+      diretorioDosMD = opcoesCLI.getDiretorioDosMD();
+      formato = opcoesCLI.getFormato();
+      arquivoDeSaida = opcoesCLI.getArquivoDeSaida();
+      modoVerboso = opcoesCLI.isModoVerboso();
 
       if ("pdf".equals(formato)) {
         try(var writer = new PdfWriter(Files.newOutputStream(arquivoDeSaida));
